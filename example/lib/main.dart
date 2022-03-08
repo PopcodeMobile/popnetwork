d011b30ca892d441a6738494946b3a91a2e8bd55
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-03-07 23:38:13 -0300

    feat: create example package

---
+++ example/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:exemple/data.dart';
+import 'package:flutter/material.dart';
+import 'package:popwork/popwork.dart';
+
+Future<void> main() async {
+  await Popwork.config(baseUrl: "https://pokeapi.co/api/v2/pokemon/");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Request'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Pokemon> list = [];
+
+  void requestApi() {
+    ApiManager.request().then((value) {
+      if (value is Success) {
+        final listMaps = value.data['results'] as List;
+        final listGenerate = List.generate(
+            listMaps.length, (index) => Pokemon.fromMap(listMaps[index]));
+        setState(() {
+          list.addAll(listGenerate);
+        });
+      }
+    });
+  }
+
+  List<Widget> get listWidget => list.isEmpty
+      ? [const Text('Not request Pokemons')]
+      : List.generate(list.length, (index) => Text(list[index].name));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: listWidget,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: requestApi,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}

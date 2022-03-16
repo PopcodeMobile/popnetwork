0109b89012b1db199a050f829985bc27fcefff11
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-03-16 13:57:56 -0300

    doc: create doc MappedApiError

--- docs/mapped_api_error.md
+++ docs/mapped_api_error.md
@@ -4,27 +4,41 @@
 
 # MappedApiError
 
-description [...]
+To map the error, it will be necessary to create a class that implements the `MappedApiError<Input, Output>` interface, it will be necessary to inform the types of input and mapping sayings;
 
 exemplo
 
 ```dart
-void main() {
-  //set config
-}
+class MapCustomError implements MappedApiError<Map, dynamic>(
+  @override
+  String get messageDefault =>
+      'Sorry, there was a problem. Please try again later.';
+  @override
+  dynamic mappingError(Map data){
+    ...
+  }
+)
 ```
+
+It is also possible to inform a standard error message, in case the application needs it.
+
+- `mappingError`: responsible for the mapping needed for your application.
+- `Input`: input that will be received from the API;
+- `Outpit`: Output that will be sent to the application, remembering that it will be displayed according to the customization.
+
 ---
-## topic
+## Configuration at startup
 <br>
-description [...]
-
+To add custom settings, just pass the edited class calling the initial network configuration.
 <br>
 
-exemplo
+examplo
 
 ```dart
 void main() {
-  //set config
+  Network.config(
+    mappedApiError: MapCustomError(),
+  )
 }
 
 ```
--- lib/src/response/states/apiError/mapped_api_error.dart
+++ lib/src/response/states/apiError/mapped_api_error.dart
@@ -1,6 +1,26 @@
-abstract class MappedApiError<I, O> {
+///To map the error, it will be necessary to create a class that implements the `MappedApiError<Input, Output>` interface, it will be necessary to inform the types of input and mapping sayings;
+/// exemplo
+/// ```dart
+/// class MapCustomError implements MappedApiError<Map, dynamic>(
+///   @override
+///   String get messageDefault =>
+///       'Sorry, there was a problem. Please try again later.';
+///   @override
+///   dynamic mappingError(Map data)
+/// )
+/// ```
+/// It is also possible to inform a standard error message, in case the application needs it.
+/// - `mappingError`: responsible for the mapping needed for your application.
+/// - `Input`: input that will be received from the API;
+/// - `Outpit`: Output that will be sent to the application, remembering that it will be displayed according to the customization.
+///
+
+abstract class MappedApiError<Input, Output> {
+  ///It is also possible to inform a standard error message, in case the application needs it.
   String get messageDefault;
-  O mappingError(I data);
+
+  ///responsible for the mapping needed for your application.
+  Output mappingError(Input data);
 }
 
 class MappedApiErrorDefault

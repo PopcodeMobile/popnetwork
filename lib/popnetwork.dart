fe1ade6e6b95016f9dffadb3762b75ccebd72fa1
Jonathan Kelvin <jnthn.klvn.s@gmail.com>
2023-10-09 14:25:14 -0300

    feat: adds extension for response success and error

--- lib/popnetwork.dart
+++ lib/popnetwork.dart
@@ -1,11 +1,11 @@
 library;
 
-export 'package:dio/dio.dart' show Response;
 export 'src/api_manager.dart';
 export 'src/i_api_manager.dart';
-export 'src/enums/http_status_enum.dart';
-export 'src/interceptors/pop_network_log_interceptor.dart';
-export 'src/interceptors/pop_cache_interceptor.dart';
-export 'src/mock/mock_reply_params.dart';
 export 'src/cache/i_cache_request_data.dart';
 export 'src/cache/request_cache_key.dart';
+export 'src/enums/http_status_enum.dart';
+export 'src/extensions/response_extension.dart';
+export 'src/interceptors/pop_cache_interceptor.dart';
+export 'src/interceptors/pop_network_log_interceptor.dart';
+export 'src/mock/mock_reply_params.dart';

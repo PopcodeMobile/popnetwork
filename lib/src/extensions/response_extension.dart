8ae41118e08cfa02726b0b6bc164532499e3624c
ThiagoMartinsPopCode <thiago.martins@popcode.com.br>
2025-07-23 12:25:55 -0300

    feat: Altera status code de sucesso de 300 para 304

--- lib/src/extensions/response_extension.dart
+++ lib/src/extensions/response_extension.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 extension ResponseExtension on Response {
   bool get isSuccessful =>
-      statusCode != null && statusCode! >= 200 && statusCode! < 300;
+      statusCode != null && statusCode! >= 200 && statusCode! < 304;
 
   bool get isError => statusCode == null || statusCode! >= 400;
 }
